4c7b579b50b80156ed36132c0f4f86327f319347
Shinemax <510360345@qq.com>
2018-08-11 20:41:11 +0800

    2

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ let http = require('http')
 let server = http.createServer(function(req,res){
   console.log('111')
   res.setHeader('Content-Type','text/plain');
-  res.end('hello')
+  res.end('hello111')
 }).listen(7000)
